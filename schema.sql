23c20f48a0dcc649839db69b1199c1e07e12d6e3
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-10-10 22:45:37 -0500

    Format code

--- schema.sql
+++ schema.sql
@@ -1,8 +1,9 @@
-create table if not exists user (
-    id integer primary key autoincrement,
-    username text not null unique,
-    xp float default 0,
-    xp_required float default 1,
-    total_xp float default 0,
-    level integer default 1
-);
+create table
+    if not exists user (
+        id integer primary key autoincrement,
+        username text not null unique,
+        xp float default 0,
+        xp_required float default 1,
+        total_xp float default 0,
+        level integer default 1
+    );
