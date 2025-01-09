8c220251435371b9338f0897337df6a8d05790d0
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-01-08 22:30:04 -0600

    Use decimals for float type

--- schema.sql
+++ schema.sql
@@ -2,8 +2,8 @@ create table
     if not exists user (
         id integer primary key autoincrement, --user id
         username text not null unique, --username
-        xp float default 0, --current user XP
-        xp_required float default 1, --user required XP to reach the next level
-        total_xp float default 0, --user total XP earned
+        xp float default 0.0, --current user XP
+        xp_required float default 1.0, --user required XP to reach the next level
+        total_xp float default 0.0, --user total XP earned
         level integer default 1 --current user level
     );
